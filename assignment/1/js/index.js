eadb3f6753492c8b60dad2572e119e8662fad834
xiong35 <2242787668@qq.com>
2020-07-09 18:06:35 +0800

    finish btn click & img scroll

--- assignment/1/js/index.js
+++ assignment/1/js/index.js
@@ -1,1 +1,33 @@
-console.log("hello");
+function $(query, all = false) {
+  if (all) {
+    return document.querySelectorAll(query);
+  }
+  return document.querySelector(query);
+}
+
+(function () {
+  const carouselDots = $(".carousel-dots button", true);
+  const carouseInfo = [
+    "Grand Theft Auto V: PlayStation 5 Announcement Trailer",
+    "Red Dead Online: Moonshiners",
+    "Grand Theft Auto Online: The Diamond Casino Heist",
+  ];
+  carouselDots.forEach((btn, ind) => {
+    btn.onclick = () => {
+      $(".carousel-footer-link span").innerHTML =
+        carouseInfo[ind];
+
+      const carouselScroller = $(".carousel-scroller");
+
+      $(".carousel-footer-link").href =
+        carouselScroller.children[ind].href;
+
+      carouselDots.forEach((btn) => {
+        btn.classList.remove("carousel-dot-active");
+      });
+      carouselDots[ind].classList.add("carousel-dot-active");
+
+      carouselScroller.style.marginLeft = -ind * 100 + "%";
+    };
+  });
+})();
