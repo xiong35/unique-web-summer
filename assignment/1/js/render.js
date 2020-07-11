a136a3b92ddc07ebc8dccc5f297da63fa9678c3c
xiong35 <2242787668@qq.com>
2020-07-11 09:06:49 +0800

    fix cursor bug & init note

--- assignment/1/js/render.js
+++ assignment/1/js/render.js
@@ -1,38 +1,37 @@
-
 async function renderPosts() {
-    const newsContainer = $(".newswire .card-container");
-  
-    let res;
-    try {
-      res = await getPosts();
-      if (res.status !== 200) {
-        raise(res);
-      }
-    } catch (err) {
-      console.log(err);
-      newsContainer.innerHTML = "<h1>something went wrong</h1>";
+  const newsContainer = $(".newswire .card-container");
+
+  let res;
+  try {
+    res = await getPosts();
+    if (res.status !== 200) {
+      raise(res);
     }
-    const posts = res.data.posts.slice(0, 6);
-  
-    const postsHTML = posts.map((it, ind) => {
-      const imgUrl = ind
-        ? it.preview_images_parsed["newswire-block-square"].src
-        : it.preview_images_parsed["newswire-block-16x9"].src;
-  
-      let flex;
-      switch (ind) {
-        case 0:
-          flex = 12;
-          break;
-        case 1:
-        case 2:
-          flex = 6;
-          break;
-        default:
-          flex = 4;
-      }
-  
-      return `
+  } catch (err) {
+    console.log(err);
+    newsContainer.innerHTML = "<h1>something went wrong</h1>";
+  }
+  const posts = res.data.posts.slice(0, 6);
+
+  const postsHTML = posts.map((it, ind) => {
+    const imgUrl = ind
+      ? it.preview_images_parsed["newswire-block-square"].src
+      : it.preview_images_parsed["newswire-block-16x9"].src;
+
+    let flex;
+    switch (ind) {
+      case 0:
+        flex = 12;
+        break;
+      case 1:
+      case 2:
+        flex = 6;
+        break;
+      default:
+        flex = 4;
+    }
+
+    return `
         <a href="${it.link}" class="card flex-${flex}">
         <div class="${ind ? "fix-1-1" : ""}">
           <img
@@ -53,29 +52,29 @@ async function renderPosts() {
               }</span>
             </div> 
         </a>`;
-    });
-  
-    newsContainer.innerHTML = postsHTML.join("");
-  }
-  
-  async function renderGames() {
-    const gameContainer = $(".featured-games .card-container");
-  
-    let res;
-    try {
-      res = await getGames();
-      if (res.status !== 200) {
-        raise(res);
-      }
-    } catch (err) {
-      console.log(err);
-      gameContainer.innerHTML = "<h1>something went wrong</h1>";
+  });
+
+  newsContainer.innerHTML = postsHTML.join("");
+}
+
+async function renderGames() {
+  const gameContainer = $(".featured-games .card-container");
+
+  let res;
+  try {
+    res = await getGames();
+    if (res.status !== 200) {
+      raise(res);
     }
-    const games = res.data.games.slice(0, 4);
-    console.log(games);
-  
-    const gameHTML = games.map((it, ind) => {
-      return `
+  } catch (err) {
+    console.log(err);
+    gameContainer.innerHTML = "<h1>something went wrong</h1>";
+  }
+  const games = res.data.games.slice(0, 4);
+  console.log(games);
+
+  const gameHTML = games.map((it, ind) => {
+    return `
       <a href="${it.link}" class="card flex-3">
         <img
           data-src="https://www.rockstargames.com/${it.fob_640}"
@@ -83,19 +82,19 @@ async function renderPosts() {
           class="lazy"
           />
           </a> `;
-    });
-  
-    gameContainer.innerHTML = gameHTML.join("");
-  }
-  
-  (async function () {
-    await renderPosts();
-    await renderGames();
-  
-    load();
-  
-    lazyLoad($(".card-container img.lazy", true));
-  
-    $(".body").classList.remove("loading");
-    $(".nav-background").style.animationPlayState = "paused";
-  })();
+  });
+
+  gameContainer.innerHTML = gameHTML.join("");
+}
+
+(async function () {
+  await renderPosts();
+  await renderGames();
+
+  load();
+
+  lazyLoad($(".card-container img.lazy", true));
+
+  $(".body").classList.remove("loading");
+  $(".nav-background").style.animationPlayState = "paused";
+})();
