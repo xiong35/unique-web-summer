f4ed7b2edc389afc4a1cd36036548879c5afcd89
xiong35 <2242787668@qq.com>
2020-07-11 11:28:42 +0800

    fix spelling mistake & add report

--- assignment/1/js/lazyload.js
+++ assignment/1/js/lazyload.js
@@ -1,30 +1,29 @@
-
 // lazy loading
 function lazyLoad(imgs) {
-    const scrollTop = document.documentElement.scrollTop;
-    const clientHeight = document.documentElement.clientHeight;
-  
-    imgs.forEach((it) => {
-      if (!it.classList.contains("lazy")) {
-        return;
-      }
-      if (it.offsetTop - scrollTop - clientHeight < 150) {
-        it.src = it.dataset.src;
-        it.classList.remove("lazy");
-      }
-    });
-  }
-  function load() {
-    const imgs = $(".card-container img.lazy", true);
-  
-    var timmer;
-    window.onscroll = () => {
-      if (timmer) {
-        return;
-      }
-      timmer = setTimeout(() => {
-        timmer = null;
-      }, 100);
-      lazyLoad(imgs);
-    };
-  }
+  const scrollTop = document.documentElement.scrollTop;
+  const clientHeight = document.documentElement.clientHeight;
+
+  imgs.forEach((it) => {
+    if (!it.classList.contains("lazy")) {
+      return;
+    }
+    if (it.offsetTop - scrollTop - clientHeight < 150) {
+      it.src = it.dataset.src;
+      it.classList.remove("lazy");
+    }
+  });
+}
+function load() {
+  const imgs = $(".card-container img.lazy", true);
+
+  var timmer;
+  window.onscroll = () => {
+    if (timmer) {
+      return;
+    }
+    timmer = setTimeout(() => {
+      timmer = null;
+    }, 100);
+    lazyLoad(imgs);
+  };
+}
