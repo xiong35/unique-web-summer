c101bdf83dcbab8b59c6d1befd8c042f0b41a724
xiong35 <2242787668@qq.com>
2020-07-26 17:47:43 +0800

    redux v2.0

--- assignment/3/myRedux.js
+++ assignment/3/myRedux.js
@@ -1,30 +1,52 @@
-let createStore = (reducer) => {
-  let state = reducer();
+class MyRedux {
+  reducer = () => {};
+  state = {};
+  listeners = [];
 
-  let listeners = [];
-  let getState = () => {
-    return state;
-  };
+  createStore = (handler) => {
+    this.reducer = handler;
+    this.state = handler();
 
-  let subscribe = (cb) => {
-    listeners.push(cb);
-    return () => {
-      listeners = listeners.filter((item) => {
-        return item !== cb;
+    this.listeners = [];
+
+    let getState = () => {
+      return this.state;
+    };
+
+    let subscribe = (curListener) => {
+      this.listeners.push(curListener);
+      return () => {
+        this.listeners = this.listeners.filter((item) => {
+          return item !== curListener;
+        });
+      };
+    };
+
+    let dispatch = (action) => {
+      this.state = this.reducer(this.state, action);
+      this.listeners.forEach((listener) => {
+        listener();
       });
+    };
+
+    let replaceReducer = (handler) => {
+      this.reducer = handler;
+    };
+
+    return {
+      getState,
+      subscribe,
+      dispatch,
+      replaceReducer,
     };
   };
 
-  let dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach((listener) => {
-      listener();
-    });
+  combineReducers = (reducers) => {
+    return (state = {}, action) => {
+      return Object.keys(reducers).reduce((nextState, key) => {
+        nextState[key] = reducers[key](state[key], action);
+        return nextState;
+      }, {});
+    };
   };
-
-  return {
-    getState,
-    subscribe,
-    dispatch,
-  };
-};
+}
